a99b79da547c8fe9f2cb36b1c0c36ccd3a91bb7d
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-08-09 23:08:27 +0300

    Update of ContractController: 1. Added security modificators -> added onlyOwner modifier 2. Added error codes to modifiers

--- smart-contracts/src/Controllers/ContractController.sol
+++ smart-contracts/src/Controllers/ContractController.sol
@@ -5,37 +5,38 @@ pragma AbiHeader pubkey;
 
 import "./interfaces/ICCCodeManager.sol";
 import "./interfaces/ICCRunLocal.sol";
+import "./interfaces/ICCMarketDeployed.sol";
 
 import "./libraries/PlatformCodes.sol";
 import "./libraries/ContractControllerErrorCodes.sol";
 
-import "../Oracle/interfaces/IOracleManageMarkets.sol";
 import "../WalletController/interfaces/IWalletControllerMarketManagement.sol";
 
 import "../utils/Platform/Platform.sol";
 import "../utils/interfaces/IUpgradableContract.sol";
 
 
-contract ContractController is IContractControllerCodeManager, IUpgradableContract, IContractControllerRunLocal {
+contract ContractController is IContractControllerCodeManager, IUpgradableContract, IContractControllerRunLocal, ICCMarketDeployed {
 
-    uint256 ownerPubkey; // TODO: owner pubkey is for tests, will be removed
     address ownerAddress;
+
+    uint32 contractCodeVersion;
 
     mapping(uint8 => CodeStorage) contractCodes;
 
     mapping(uint8 => address[]) deployedContracts;
     mapping(address => uint8) knownContracts;
     // Contract is deployed as regular contract
-    constructor() public {
+    constructor(address ownerAddress_) public {
         tvm.accept();
+        ownerAddress = ownerAddress_;
+        contractCodeVersion = 0;
     }
 
     /*
         Data for upgrade from version 0 to version 1:
         data:
             bits:
-                uint256 ownerPubkey
-                address ownerAddress
                 uint32 codeVersion
             refs:
                 1. mapping refs
@@ -45,17 +46,16 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
                         3. mapping(address => uint8) knownContracts
      */
     // From version 0 to version 1
-    function upgradeContractCode(TvmCell code, TvmCell updateParams, uint32 codeVersion_, uint8 contractType_) override external correctContractType(contractType_) newVersion(codeVersion_) {
+    function upgradeContractCode(TvmCell code, TvmCell updateParams, uint32 codeVersion_, uint8 contractType_) override external onlyOwner correctContractType(contractType_) newVersion(contractCodeVersion, codeVersion_) {
         tvm.accept();
         TvmBuilder dataBuilder;
-        dataBuilder.store(ownerPubkey);
         dataBuilder.store(ownerAddress);
         dataBuilder.store(codeVersion_);
 
         TvmBuilder mappingBuilder;
         TvmBuilder contractCodesStorage;
         TvmBuilder deployedContractsStorage;
-        TvmBuidler knownContractsStorage;
+        TvmBuilder knownContractsStorage;
         contractCodesStorage.store(contractCodes);
         deployedContractsStorage.store(deployedContracts);
         knownContractsStorage.store(knownContracts);
@@ -85,7 +85,7 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
      * @param codeVersion Version of contract code
      * @param deployCost Cost of deploying contract in nanotons
      */
-    function addContractCode(uint8 contractType, TvmCell code, uint32 codeVersion, uint128 deployCost) override external contractTypeExists(contractType, false) {
+    function addContractCode(uint8 contractType, TvmCell code, uint32 codeVersion, uint128 deployCost) override external onlyOwner contractTypeExists(contractType, false) {
         tvm.accept();
         contractCodes[contractType] = CodeStorage(code, codeVersion, deployCost);
     }
@@ -95,7 +95,7 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
      * @param code Updated contract code
      * @param codeVersion New code version
      */
-    function updateContractCode(uint8 contractType, TvmCell code, uint32 codeVersion) override external contractTypeExists(contractType, true) {
+    function updateContractCode(uint8 contractType, TvmCell code, uint32 codeVersion) override external onlyOwner contractTypeExists(contractType, true) {
         tvm.accept();
         if (codeVersion > contractCodes[contractType].codeVersion) {
             contractCodes[contractType].code = code;
@@ -107,7 +107,7 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
      * @param contractType Type of contract
      * @param deployCost Cost of deploying contract in nanotons
      */
-    function setContractDeployCost(uint8 contractType, uint128 deployCost) override external contractTypeExists(contractType, true) {
+    function setContractDeployCost(uint8 contractType, uint128 deployCost) override external onlyOwner contractTypeExists(contractType, true) {
         tvm.accept();
         contractCodes[contractType].deployCost = deployCost;
     }
@@ -117,7 +117,7 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
      * @param initialData InitialData for deployed smart contract
      * @param params Parameters for smart contract deployment
      */
-    function createContract(uint8 contractType, TvmCell initialData, TvmCell params) override external responsible contractTypeExists(contractType, true) returns (address) {
+    function createContract(uint8 contractType, TvmCell initialData, TvmCell params) override external responsible creator contractTypeExists(contractType, true) returns (address) {
         require(msg.value >= contractCodes[contractType].deployCost, ContractControllerErrorCodes.ERROR_MSG_VALUE_LOW);
         tvm.accept();
         address newContract = new Platform{
@@ -140,7 +140,7 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
      * @param contractAddress Address of smart contract to update
      * @param updateParams Parameters required for update
      */
-    function updateContract(uint8 contractType, address contractAddress, TvmCell updateParams) override external contractTypeExists(contractType, true) {
+    function updateContract(uint8 contractType, address contractAddress, TvmCell updateParams) override external onlyOwner contractTypeExists(contractType, true) {
         tvm.accept();
         IUpgradableContract(contractAddress).upgradeContractCode{
             value: msg.value,
@@ -149,10 +149,10 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
     }
 
     /**
-     * @param conractType Type of contracts to update
+     * @param contractType Type of contracts to update
      * @param updateParams Parameters passed to contracts during update
      */
-    function updateContracts(uint8 contractType, TvmCell updateParams) override external {
+    function updateContracts(uint8 contractType, TvmCell updateParams) override external onlyOwner contractTypeExists(contractType, true) {
         tvm.accept();
         TvmCell code = contractCodes[contractType].code;
         uint32 codeVersion = contractCodes[contractType].codeVersion;
@@ -168,11 +168,11 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
      * @param realTokenRoot Address of market's real token (ex. wTON)
      * @param virtualTokenRoot Address of market's virtual token (ex. vTON)
      */
-    function marketDeployed(address realTokenRoot, address virtualTokenRoot) external override onlyKnownContract(ContractCodes.MARKET) {
+    function marketDeployed(address realTokenRoot, address virtualTokenRoot) external override onlyKnownContract(PlatformCodes.MARKET) {
         tvm.accept();
         address market = msg.sender;
 
-        for (address walletController: deployedContracts[ContractCodes.WALLET_CONTROLLER]) {
+        for (address walletController: deployedContracts[PlatformCodes.WALLET_CONTROLLER]) {
             IWalletControllerMarketManagement(walletController).addMarket(market, realTokenRoot, virtualTokenRoot);
         }
     }
@@ -221,6 +221,11 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
 
     /*********************************************************************************************************/
     // modifiers
+    modifier onlyOwner() {
+        require(msg.sender == ownerAddress, ContractControllerErrorCodes.ERROR_MSG_SENDER_IS_NOT_ROOT);
+        _;
+    }
+
     /**
      * @param contractType Type of contract
      * @param exists Does contractType exist in contractCodes mapping
@@ -229,20 +234,39 @@ contract ContractController is IContractControllerCodeManager, IUpgradableContra
         require(contractCodes.exists(contractType) == exists, exists == true? ContractControllerErrorCodes.ERROR_CONTRACT_TYPE_DOES_NOT_EXIST : ContractControllerErrorCodes.ERROR_CONTRACT_TYPE_ALREADY_EXISTS);
         _;
     }
-
-    // TODO: add desc + error codes
+    
+    /**
+     * @param contractType Type of contract
+     */
     modifier correctContractType(uint8 contractType) {
-        require(contractType == PlatformCodes.CONTRACT_CONTROLLER);
+        require(contractType == PlatformCodes.CONTRACT_CONTROLLER, ContractControllerErrorCodes.ERROR_CONTRACT_TYPE_IS_INVALID);
         _;
     }
 
+    /**
+     * @notice Check if msg.sender is presented in mapping knownContracts
+     * @param contractType Type of contract
+     */
     modifier onlyKnownContract(uint8 contractType) {
-        require(knownContracts[msg.sender] == contractType);
+        require(knownContracts[msg.sender] == contractType, ContractControllerErrorCodes.ERROR_MSG_SENDER_IS_NOT_KNOWN);
+        _;
+    }
+
+    modifier creator() {
+        require(msg.sender == ownerAddress || knownContracts[msg.sender] == PlatformCodes.USER_ACCOUNT_MANAGER, ContractControllerErrorCodes.ERROR_MSG_SENDER_IS_NOT_CREATOR);
+        _;
+    }
+
+    modifier newVersion(uint32 oldCodeVersion, uint32 newCodeVersion) {
+        require(oldCodeVersion < newCodeVersion, ContractControllerErrorCodes.ERROR_CODE_VERSION_IS_NOT_UPDATED);
         _;
     }
 
     /*********************************************************************************************************/
-    // For local execution
+    /**
+     * @param pubkey Owner pubkey
+     * @param addr Owner address
+     */
     function createInitialDataForOracle(uint256 pubkey, address addr) external override returns(TvmCell) {
         TvmBuilder initialData;
         initialData.store(pubkey, addr);
