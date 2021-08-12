913cffa6380fa4b62a83eee5e76229b9b98015a3
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-08-12 04:23:39 +0300

    Added functions to ContractController: 1. Functions for creating initial data for contract deploy 2. Functions for creating parameters for contract deploy
    Changes in Market:
    1. Changed onCodeUpgrade -> removed parameters swapPairAddress and isLeft (leftover after Oracle rework)

--- smart-contracts/src/Controllers/interfaces/ICCRunLocal.sol
+++ smart-contracts/src/Controllers/interfaces/ICCRunLocal.sol
@@ -1,9 +1,21 @@
 pragma ton-solidity >= 0.39.0;
 
 interface IContractControllerRunLocal {
-    function createInitialDataForOracle(uint256 pubkey, address addr) external returns(TvmCell);
+    function createInitialDataForMarket(address tokenRoot, address tip3Deployer, address walletController, address oracle) external returns (TvmCell);
+    function createParamsForMarket() external returns (TvmCell);
 
-    function createInitialDataForUserAccountManager() external returns(TvmCell);
+    function createInitialDataForOracle(uint256 pubkey, address addr) external returns (TvmCell);
+    function createParamsForOracle() external returns (TvmCell);
 
-    function createInitialDataForWalletController() external returns(TvmCell);
+    function craeteInitialDataForTIP3Deployer(address ownerAddress_) external returns (TvmCell);
+    function craeteParamsForTIP3Deployer() external returns (TvmCell);
+
+    function createInitialDataForUserAccount(address msigOwner) external returns (TvmCell);
+    function createParamsForUserAccount() external returns (TvmCell);
+
+    function createInitialDataForUserAccountManager() external returns (TvmCell);
+    function createParamsForUserAccountManager() external returns (TvmCell);
+
+    function createInitialDataForWalletController() external returns (TvmCell);
+    function createParamsForWalletController() external returns (TvmCell);
 }
