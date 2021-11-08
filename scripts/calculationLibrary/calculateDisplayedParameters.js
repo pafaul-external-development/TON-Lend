5c62926e8c8032babb29f7b42538efc29247e0c5
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-11-08 22:31:43 +0300

    upd

---
+++ scripts/calculationLibrary/calculateDisplayedParameters.js
@@ -0,0 +1,167 @@
+const secondsPerYear = 365 * 24 * 60 * 60;
+
+/**
+ * @typedef BorrowInfo
+ * @type {Object}
+ * @property {String} tokensBorrowed
+ * @property {Fraction} index
+ */
+
+/**
+ * @typedef Fraction
+ * @type {Object}
+ * @property {String} nom
+ * @property {String} denom
+ */
+
+/**
+ * @typedef MarketInfo
+ * @type {Object}
+ * @property {String} token
+ * @property {String} realTokenBalance
+ * @property {String} vTokenBalance
+ * @property {String} totalBorrowed
+ * @property {String} totalReserve
+ * @property {Fraction} index
+ * @property {Fraction} baseRate
+ * @property {Fraction} utilizationMultiplier
+ * @property {Fraction} reserveFactor
+ * @property {Fraction} exchangeRate
+ * @property {Fraction} collateralFactor
+ * @property {Fraction} liquidationMultiplier
+ * @property {String} lastUpdateTime
+ */
+
+/**
+ * @typedef AllMarketsInfo
+ * @type {Record<String, MarketInfo>}
+ */
+
+/**
+ * @typedef UserMarketInfo
+ * @type {Object}
+ * @property {Boolean} exists
+ * @property {Number} _marketId
+ * @property {String} suppliedTokens
+ * @property {Fraction} getAccountHealth
+ * @property {BorrowInfo} borrowInfo
+ */
+
+/**
+ * @typedef UserInfo
+ * @type {Record<String, UserMarketInfo>}
+ */
+
+/**
+ * @typedef Prices
+ * @type {Record<String, Fraction>}
+ */
+
+/**
+ * 
+ * @param {Fraction} f 
+ * @returns 
+ */
+function f(f) {
+    return Number(f.nom) / Number(f.denom);
+}
+
+/**
+ * @param {Object} param0 
+ * @param {AllMarketsInfo} param0.markets
+ * @param {UserInfo} param0.userInfo
+ * @param {Prices} param0.prices
+ */
+function getUSDBorrowed({
+    markets,
+    userInfo,
+    prices
+}) {
+    let borrowedSum = 0;
+    for (let marketId in userInfo) {
+        borrowedSum += 
+            Number(userInfo[marketId].borrowInfo.tokensBorrowed) / 
+            f(userInfo[marketId].borrowInfo.index) *
+            f(markets[marketId].index) / 
+            f(prices[markets[marketId].token]);
+    }
+
+    return borrowedSum;
+}
+
+/**
+ * @param {Object} param0 
+ * @param {AllMarketsInfo} param0.markets
+ * @param {UserInfo} param0.userInfo
+ * @param {Prices} param0.prices
+ */
+function getUSDCollateral({
+    markets,
+    userInfo,
+    prices
+}) {
+    let collateral = 0;
+    for (let marketId in userInfo) {
+        collateral += 
+            Number(userInfo[marketId].suppliedTokens) * 
+            f(markets[marketId].exchangeRate) /
+            f(prices[markets[marketId].token]) *
+            f(markets[marketId].collateralFactor);
+    }
+}
+
+/**
+ * @param {Object} param0 
+ * @param {AllMarketsInfo} param0.markets
+ * @param {UserInfo} param0.userInfo
+ * @param {Prices} param0.prices
+ */
+function getAccountHealth({
+    markets,
+    userInfo,
+    prices
+}) {
+    let borrowedSum = getUSDBorrowed({markets, userInfo, prices});
+    let suppliedSum = getUSDCollateral({markets, userInfo, prices});
+    let usedPercentage = borrowedSum / suppliedSum;
+    let accountHealth = (1 - usedPercentage) * 100;
+    return accountHealth;
+}
+
+/**
+ * 
+ * @param {Object} param0 
+ * @param {Number} param0.collateralValue
+ * @param {Number} param0.borrowedValue
+ * @param {Number} param0.tokenPrice
+ */
+function getPossibleBorrow({
+    collateralValue, 
+    borrowedValue,
+    tokenPrice
+}) {
+    if (collateralValue > borrowedValue)
+        return (collateralValue - borrowedValue) * tokenPrice;
+    else
+        return 0;
+}
+
+/**
+ * 
+ * @param {Object} param0 
+ * @param {MarketInfo} param0.market
+ */
+function getCurrentAPY({
+    market
+}) {
+    let currentRate = Number(market.baseRate) + Number(market.totalBorrowed) / (Number(market.totalBorrowed) + Number(market.realTokenBalance)) * f(market.utilizationMultiplier);
+    return currentRate * secondsPerYear;
+}
+
+module.exports = {
+    getUSDBorrowed,
+    getUSDCollateral,
+    getAccountHealth,
+    getPossibleBorrow,
+    getCurrentAPY
+}
