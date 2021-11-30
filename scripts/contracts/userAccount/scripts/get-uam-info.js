0a9b188248cd7b07ce81de10cc2489fbfe9a231f
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-11-30 19:19:38 +0300

    Update + added withdraw ton script

--- scripts/contracts/userAccount/scripts/get-uam-info.js
+++ scripts/contracts/userAccount/scripts/get-uam-info.js
@@ -8,7 +8,7 @@ async function main() {
     });
 
     console.log(`Owner: ${await contracts.userAccountManager.owner()}`);
-    
+
     console.log(`Market address: ${await contracts.userAccountManager.marketAddress()}`);
 
     console.log(`Modules: ${pp(await contracts.userAccountManager.modules())}`);
@@ -20,6 +20,8 @@ async function main() {
     console.log(`Contract code version: ${await contracts.userAccountManager.contractCodeVersion()}`);
 
     console.log(`Contract codes available: ${pp(await contracts.userAccountManager.userAccountCodes())}`);
+
+    console.log(`User account address: ${await contracts.userAccountManager.calculateUserAccoutAddress({tonWallet: '0:b16223d7abbd15b07fbcb31bf9281c76e8cdf79cd0aabd29330fc7483ac00c9e'})}`);
 }
 
 main().then(
